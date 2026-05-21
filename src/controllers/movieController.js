76a05c26915e8d19f02cc8e20fc68d3025b164cb
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-21 13:16:24 +0200

    change movie controller with helper functions for MongoDB operations

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,60 +1,62 @@
-import { ObjectId } from "mongodb";
+// Helpers for accessing the MongoDB collection and converting string IDs to ObjectId
+const getCollection = request => request.server.mongo.db.collection("movies");
+const toObjectId = (request, id) => new request.server.mongo.ObjectId(id);
 
+// GET /movies - Get all movies
 export async function getAllMovies(request, reply) {
-    const movies = await request.server.mongo.db
-        .collection("movies")
-        .find()
-        .toArray();
 
+    const movies = await getCollection(request).find().toArray();
     reply.send(movies);
 }
 
+// GET /movies/:id - Get a movie by ID
 export async function getMovieById(request, reply) {
+    const movie = await getCollection(request).findOne({
+        _id: toObjectId(request, request.params.id)
+    });
 
-    const movie = await request.server.mongo.db
-        .collection("movies")
-        .findOne({ _id: new ObjectId(request.params.id) });
-
+    // check if the movie exists, if not return 404
     if (!movie) {
-        reply.status(404).send({ message: "Filmen hittades inte" });
-        return;
+        return reply.status(404).send({ message: "Filmen hittades inte" });
     }
     return reply.send(movie);
 }
 
+// POST /movies - Add a new movie
 export async function addMovie(request, reply) {
-    const newMovie = await request.server.mongo.db
-        .collection("movies")
-        .insertOne(request.body);
+    const newMovie = await getCollection(request).insertOne(request.body);
 
-    return {
+    // return 201 Created with the new movie's ID
+    return reply.status(201).send({
         message: "Filmen har lagts till",
         movieId: newMovie.insertedId,
-    }
+    });
 }
 
+// PUT /movies/:id - Update a movie by ID
 export async function updateMovie(request, reply) {
+    const updated = await getCollection(request).updateOne(
+        { _id: toObjectId(request, request.params.id) },
+        { $set: request.body }      // updates only the fields provided in the request body
+    );
 
-    const updated = await request.server.mongo.db
-        .collection("movies")
-        .updateOne(
-            { _id: new ObjectId(request.params.id) },
-            { $set: request.body }
-        );
-    if (!updated) {
+    // check if any document was matched for update, if not return 404
+    if (updated.matchedCount === 0) {
         return reply.status(404).send({ message: "Filmen hittades inte" });
     }
 
-    return reply.send({ message: "Filmen har uppdaterats", movie: updated });
+    // return success message if the movie was updated
+    return reply.send({ message: "Filmen har uppdaterats" });
 }
 
+// DELETE /movies/:id - Delete a movie by ID
 export async function deleteMovie(request, reply) {
+    const deleted = await getCollection(request).deleteOne({
+        _id: toObjectId(request, request.params.id)
+    });
 
-    const deleted = await request.server.mongo.db
-        .collection("movies")
-        .deleteOne({ _id: new ObjectId(request.params.id) });
-
-    if (!deleted) {
+    // check if any document was deleted, if not return 404
+    if (deleted.deletedCount === 0) {
         return reply.status(404).send({ message: "Filmen hittades inte" });
     }
     return reply.send({ message: "Filmen har tagits bort" });
