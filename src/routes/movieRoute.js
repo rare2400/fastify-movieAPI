023d897c8518d2147a9e8f2146ac114b741b2e08
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-21 10:58:20 +0200

    add movie routes with CRUD operations and validation

---
+++ src/routes/movieRoute.js
@@ -0,0 +1,24 @@
+import * as movieController from '../controllers/movieController.js';
+import { movieSchema } from '../schemas/movieSchema.js';
+
+export default async function movieRoutes(fastify) {
+
+    // Get all movies
+    fastify.get('/movies', movieController.getAllMovies);
+
+    // Get one movie by ID
+    fastify.get('/movies/:id', movieController.getMovieById);
+
+    // Add a new movie with validation
+    fastify.post('/movies', {
+        schema: { body: movieSchema }
+    }, movieController.addMovie);
+
+    // Update an existing movie with validation
+    fastify.put('/movies/:id', {
+        schema: { body: movieSchema }
+    }, movieController.updateMovie);
+
+    // Delete a movie by ID
+    fastify.delete('/movies/:id', movieController.deleteMovie);
+}
