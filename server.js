9ecd7394df374d743f6c862b0ad17b180966f106
Ramona Reinholdz <rare2400@student.miun.se>
2026-05-21 13:26:57 +0200

    configure Fastify server with environment variables and register plugins

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 'use strict';
 
-import Fastify from 'fastify';
+import Fastify from "fastify";
+import dotenv from "dotenv";
+import mongoPlugin from "./src/plugins/mongo.js";
+import movieRoutes from "./src/routes/MovieRoute.js";
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Fastify server instance with logging enabled
 const fastify = Fastify({ logger: true });
-const PORT = 3000;
 
 
+// Register plugins and routes
+await fastify.register(mongoPlugin);
+await fastify.register(movieRoutes);
+
+// Start the server with error handling
 const start = async () => {
+  const port =  process.env.PORT || 3000;
+
   try {
-    await fastify.listen(PORT);
-    console.log(`Server is running on port ${PORT}`);
+    await fastify.listen({ port: port });
+    console.log(`Server is running on port ${ port }`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
